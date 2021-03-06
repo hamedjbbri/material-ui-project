84f0b0da133788e05552ac69db31f9d845d57e38
Hamed Jabbari <jabbary.hamed@gmail.com>
2021-03-06 10:13:16 -0800

    Footer and responsive

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Typography, AppBar, Button, Card, CardActions, CardContent, CardMedia, 
 import { PhotoCamera } from '@material-ui/icons';
 import useStyles from './styles';
 
+const cards = [1,2,3,4,5,6,7,8,9]
 
 const App = () => {
     const classes = useStyles();
@@ -11,7 +12,7 @@ const App = () => {
             <CssBaseline />
             <AppBar position="relative">
                <Toolbar>
-                   <PhotoCamera />
+                   <PhotoCamera className={classes.icon} />
                    <Typography variant="h6">
                        Photo Album
                    </Typography>
@@ -26,7 +27,7 @@ const App = () => {
                        <Typography variant="h5" align="center" color="textSecondary" paragraph>
                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui neque architecto sed reiciendis quo soluta, earum recusandae est odit tempore, fuga cumque modi vel. Provident quas voluptas assumenda quaerat nam.
                        </Typography>
-                       <div>
+                       <div className={classes.buttons}>
                            <Grid container spacing={2} justify="center">
                                 <Grid item>
                                     <Button variant="contained" color="primary">
@@ -42,9 +43,49 @@ const App = () => {
                        </div>
                     </Container>
                 </div>
+
+                <Container className={classes.cardGrid} maxWidth="md">
+                    <Grid container spacing={4}>
+                        {cards.map((card) =>(
+                        <Grid item key={card} xs={12} sm={6} md={4}>
+                            <Card className={classes.card}> 
+                                <CardMedia 
+                                   className={classes.cardMedia}
+                                   image="https://source.unsplash.com/random"
+                                   title="image title"    
+                                />
+                                <CardContent classNam={classes.cardContent}>  
+                                    <Typography gutterBottom variant="h5"> 
+                                           Heading
+                                    </Typography>
+                                    <Typography>
+                                        This is a media card you can use the section for describe the content
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small" color="primary">
+                                        View
+                                    </Button>
+                                    <Button size="small" color="primary">
+                                        Edit
+                                    </Button>
+                                </CardActions>
+                            </Card> 
+                        </Grid>
+
+                        ))}
+                    </Grid>
+                </Container>
             </main>
-             
-        </>    
+            <footer className={classes.footer}>
+                   <Typography variant="h6" align="center" gutterBottom>
+                      Footer
+                   </Typography>
+                   <Typography variant="subtitle1" align="center" color="textSecondary">
+                      Hamed jabbari
+                   </Typography>
+            </footer>    
+    </>
     )
 }
 
